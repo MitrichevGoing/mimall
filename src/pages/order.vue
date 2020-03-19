bf82a2b84805c221c57d88931047c80da5da529c
potter <potter@potterdeMacBook-Pro.local>
2020-03-19 20:34:27 +0800

    路由封装

---
+++ src/pages/order.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        order
+        <order-header></order-header>
+        <router-view></router-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+    import NavFooter from './../components/NavFooter'
+    import OrderHeader from './../components/OrderHeader'
+    export default {
+        name: 'nav-home',
+        components:{
+            OrderHeader,
+            NavFooter
+        }
+    }
+</script>
